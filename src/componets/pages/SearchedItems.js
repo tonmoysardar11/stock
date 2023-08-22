4a35aa72b7c6cf1756d6e1051681bfa52f69d2d3
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-08-22 18:25:19 +0530

    Searchbar tweaks

--- src/componets/pages/SearchedItems.js
+++ src/componets/pages/SearchedItems.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { stockSearch } from "../../api/stock-api";
+import { StockContext } from "../../context/StockContext";
 
 const SearchedItems = ({ find }) => {
   const [data, setdata] = useState([]);
   const [loading, setloading] = useState(false);
   const { theme } = useContext(ThemeContext);
+  const {setstock}= useContext(StockContext)
 
   const load = async () => {
     setloading(true)
@@ -34,7 +36,7 @@ const SearchedItems = ({ find }) => {
     <>
       <div
         className={`absolute top-14 left-5 md:left-[28vw] max-h-[50vh] overflow-y-auto z-30 mt-2 w-[80vw] md:w-[26vw] origin-top-right rounded-md ${
-          !theme ? "bg-white text-black" : "bg-black text-white"
+          !theme ? "bg-white text-black" : "bg-black text-white shadow-md shadow-gray-500"
         } shadow-2xl ring-1 ring-black ring-opacity-5`}
         role="menu"
         aria-orientation="vertical"
@@ -47,7 +49,7 @@ const SearchedItems = ({ find }) => {
                   <div
                     key={index}
                     className="block px-4 py-2 text-sm cursor-pointer flex justify-between items-center hover:font-semibold"
-                    onClick={''}
+                    onClick={()=>setstock(element.displaySymbol)}
                   >
                     <p>{element.description}</p>
                     <p>{element.displaySymbol}</p>
